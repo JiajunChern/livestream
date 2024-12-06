3d5cbb696b346e2d30e042d90d2ac5df44987c37
Jiajun Chen <chenjiajun@hnu.edu.cn>
2024-12-06 22:32:53 +0800

    feat: 增加 t_pay_product 表数据 SQL

--- sql/livestream-bank.sql
+++ sql/livestream-bank.sql
@@ -27,6 +27,12 @@ CREATE TABLE `t_pay_product` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='付费产品表';
 
+INSERT INTO `t_pay_product` VALUES (6, '30元虚拟币', 3000, '{\"coin\":300,\"url\":\"www.tttt.com\"}', 0, 1, '2024-02-22 16:32:28', '2024-02-22 16:56:22');
+INSERT INTO `t_pay_product` VALUES (7, '35元虚拟币', 3500, '{\"coin\":350,\"url\":\"www.tttt.com\"}', 0, 1, '2024-02-22 16:32:28', '2024-02-22 16:57:13');
+INSERT INTO `t_pay_product` VALUES (8, '40元虚拟币', 4000, '{\"coin\":400,\"url\":\"www.tttt.com\"}', 0, 1, '2024-02-22 16:32:28', '2024-02-22 16:57:18');
+INSERT INTO `t_pay_product` VALUES (9, '50元虚拟币', 5000, '{\"coin\":500,\"url\":\"www.tttt.com\"}', 0, 1, '2024-02-22 16:32:28', '2024-02-22 16:57:21');
+INSERT INTO `t_pay_product` VALUES (10, '100元虚拟币', 10000, '{\"coin\":1000,\"url\":\"www.tttt.com\"}', 0, 1, '2024-02-22 16:32:28', '2024-02-22 16:57:26');
+
 -- Create syntax for TABLE 't_pay_topic'
 CREATE TABLE `t_pay_topic` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
